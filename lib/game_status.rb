acf5f1b3e9176e9d04fff58767a07d6449377b54
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-27 03:54:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,7 @@ def won?(board)
     return [3, 4, 5]
   elsif (board[6] == "X" && board[7] == "X" && board[8] == "X") || (board[6] == "O" && board[7] == "O" && board[8] == "O")
     return [6, 7, 8]
-  if (board[0] == "X" && board[3] == "X" && board[6] == "X") || (board[0] == "O" && board[3] == "O" && board[6] == "O")
+  elsif (board[0] == "X" && board[3] == "X" && board[6] == "X") || (board[0] == "O" && board[3] == "O" && board[6] == "O")
     return [0, 3, 6]
   elsif (board[1] == "X" && board[4] == "X" && board[7] == "X") || (board[1] == "O" && board[4] == "O" && board[7] == "O")
     return [1, 4, 7]
