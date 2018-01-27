75af47e13479393144cead4fcd7e6ead5a98724c
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-27 03:51:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,8 +7,15 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
 def won?(board)
-  if board = [" "," "," "," "," "," "," "," "," "]
+  if (board[0] == "X" && board[1] == "X" && board[2] == "X")
+    return [0, 1, 2]
+  elsif (board[3] == "X" && board[4] == "X" && board[5] == "X")
+    return [3, 4, 5]
+  elsif (board[6] == "X" && board[7] == "X" && board[8] == "X")
+    return [6, 7, 8]
+  else
     return false
   end
 
 end
+
